93204cc1b1c3e67a86a3c514b917c41f7909ed2f
Rohit <rjdhav67@gmail.com>
2026-02-18 02:10:25 +0530

    auth

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,11 @@
+import { scalekit } from "@/lib/scalekit";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req:NextRequest) {
+   const redirectUri= `${process.env.NEXT_PUBLIC_SCALEKIT_REDIRECT_URI}/api/auth/callback`
+   const url=scalekit.getAuthorizationUrl(redirectUri)
+   console.log(url)
+   return NextResponse.redirect(url)
+
+   
+}
