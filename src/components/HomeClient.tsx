4643de4b4350b19a4fdc82c96d3a927604c34c03
Rohit <rjdhav67@gmail.com>
2026-02-24 01:47:47 +0530

    Home page: changes in hero section

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -53,7 +53,27 @@ function HomeClient({ email }: { email: string }) {
 
         </div>
       </motion.div>
-    section
+    <section className='pt-36 pb-28 px-6'>
+     <div className='max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-20 items-center'>
+       <motion.div
+       initial={{ opacity: 0, x: -40 }}
+       animate={{ opacity: 1, x: 0 }}
+       transition={{ duration: 0.7 }}
+       className=''>
+        <h1 className='text-4xl md:text-5xl font-semibold tracking-tight'>
+          AI Customer Support <br /> Build for Modern Websites
+        </h1>
+        <p className='mt-6 text-lg text-zinc-600 max-w-xl '>
+          Add a powerfull AI Chatbot to your website in minutes. Our AI Customer Support is designed to provide instant and accurate responses to your customers, improving their experience and boosting your business. With easy integration and customizable features, you can have your AI support up and running in no time.
+        </p>
+
+       </motion.div>
+       <div>
+
+       </div>
+
+     </div>
+    </section>
     </div>
   )
 }
