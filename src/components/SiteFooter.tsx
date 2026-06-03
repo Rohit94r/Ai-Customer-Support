abceef76656c817fe055883ecfc084d9b54562df
Rohit Jadhav <rjdhav67@gmail.com>
2026-06-03 18:17:51 +0530

    Update SiteFooter component to shorten the name displayed from "Rohit Jadhav" to "Rohit".

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -66,7 +66,7 @@ export default function SiteFooter() {
             rel="noopener noreferrer"
             target="_blank"
           >
-            Rohit Jadhav
+            Rohit
           </a>
           .
         </p>
